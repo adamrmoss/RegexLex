9bdf948c77fa4a986f777089081ab283c39f1740
Adam Moss <adamrmoss@me.com>
2013-03-27 22:14:18 -0400

    Turned executable "demo" into first test

---
+++ SimpleLexer.Core/ILexer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleLexer.Core
+{
+  public interface ILexer
+  {
+    void AddDefinition(TokenDefinition tokenDefinition);
+
+    IEnumerable<Token> Tokenize(string source);
+  }
+}
---
+++ SimpleLexer.Core/TokenDefinition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SimpleLexer.Core
+{
+  public class TokenDefinition
+  {
+    public TokenDefinition(
+            string type,
+            Regex regex)
+            : this(type, regex, false)
+    {
+    }
+
+    public TokenDefinition(
+            string type,
+            Regex regex,
+            bool isIgnored)
+    {
+      Type = type;
+      Regex = regex;
+      IsIgnored = isIgnored;
+    }
+
+    public bool IsIgnored { get; private set; }
+
+    public Regex Regex { get; private set; }
+
+    public string Type { get; private set; }
+  }
+}
