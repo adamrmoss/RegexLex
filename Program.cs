4fea54571802469b228c72bd5afda1d23bf1c077
Adam Moss <adamrmoss@me.com>
2013-03-27 21:30:12 -0400

    Initial version

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SimpleLexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lexer = new Lexer();
+
+            lexer.AddDefinition(new TokenDefinition(
+                "(operator)",
+                new Regex(@"\*|\/|\+|\-")));
+
+            lexer.AddDefinition(new TokenDefinition(
+                "(literal)",
+                new Regex(@"\d+")));
+
+
+            lexer.AddDefinition(new TokenDefinition(
+                "(white-space)",
+                new Regex(@"\s+"),
+                true));
+
+            var tokens = lexer.Tokenize("1 * 2 / 3 + 4 - 5");
+            
+            foreach (var token in tokens)
+                Console.WriteLine(token);
+
+            Console.WriteLine("Press ENTER to quit.");
+            Console.ReadLine();
+        }
+    }
+}
